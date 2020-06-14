e21a44bf2114d6f4d1ee7088fcdafcc297f4cf8f
ordericvit <orderic.vit@gmail.com>
2020-06-14 21:26:17 +0300

    Update main.tf

--- prod/main.tf
+++ prod/main.tf
@@ -4,18 +4,18 @@ provider "google" {
 }
 
 module "app" {
-  source          = "C:/Users/Orderic Vitalinius/infra/terraform/prod/modules/app"
+  source          = "C:/Users/test/infra/terraform/prod/modules/app"
   public_key_path = "${var.public_key_path}"
   app_disk_image  = "${var.app_disk_image}"
 }
 
 module "db" {
-  source          = "C:/Users/Orderic Vitalinius/infra/terraform/prod/modules/db"
+  source          = "C:/Users/test/infra/terraform/prod/modules/db"
   public_key_path = "${var.public_key_path}"
   db_disk_image   = "${var.db_disk_image}"
 }
 
 module "vpc" {
-  source        = "C:/Users/Orderic Vitalinius/infra/terraform/prod/modules/vpc"
+  source        = "C:/Users/test/infra/terraform/prod/modules/vpc"
   source_ranges = "${var.ip_range}"
 }
